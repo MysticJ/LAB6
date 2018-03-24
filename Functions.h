e360c1d4e641f915d348578f93020c63f34b14f6
ShirleyQian <sqianab@connect.ust.hk>
2018-03-25 01:06:14 +0800

    Bomb added

--- Functions.h
+++ Functions.h
@@ -12,6 +12,7 @@
 #include <cstdlib>
 using namespace std;
 
+void CheckWin(){}
 
 void CheckCrash(bool& crash, int count, int Me_Row, int Me_Col, int Mon_Row[], int Mon_Col[]){
 	for (int i=0; i<=count; i++){
@@ -25,69 +26,114 @@ void CheckCrash(bool& crash, int count, int Me_Row, int Me_Col, int Mon_Row[], i
 void NewMonster(int Mon_Row[], int Mon_Col[], int count, int Me_Row, int Me_Col){
 	int row=rand()%15;
 	int col=rand()%15;
-//	if (((row==14)&&(col==0))||((row==0)&&(col==14))||((row==Me_Row)&&(col==Me_Col))){
-//		row=rand()%15;
-//		col=rand()%15;
-//	}
+	while (((row==14)&&(col==0))||((row==0)&&(col==14))||((row==Me_Row)&&(col==Me_Col))){
+		row=rand()%15;
+		col=rand()%15;
+	}
 	Mon_Row[count]=row;
 	Mon_Col[count]=col;
 	cout<<" new monster row:"<<Mon_Row[count]+1<<" col:"<<Mon_Col[count]+1<<endl;
 
 }
-
+// move all the monsters randomly
 void MoveMonster(int Mon_Row[], int Mon_Col[], int count){
 	for (int i=0; i<count; i++){
 		int addrow=rand()%3-1;
 		int addcol=rand()%3-1;
-		while ((addrow+addcol!=1)||(addrow+addcol!=-1)){
-			addrow=rand()%2;
-			addcol=rand()%2;
+		while ((addrow==0)&&(addcol==0)){
+			addrow=rand()%3-1;
+			addcol=rand()%3-1;
 		}
-		Mon_Row[i]=((Mon_Row[i]+addrow==0)||(Mon_Row[i]+addrow==14))? Mon_Row[i]:Mon_Row[i]+addrow;
-		Mon_Col[i]=((Mon_Col[i]+addcol==0)||(Mon_Col[i]+addcol==14))? Mon_Col[i]:Mon_Col[i]+addcol;
+		Mon_Row[i]=((Mon_Row[i]+addrow==-1)||(Mon_Row[i]+addrow==15))? Mon_Row[i]:Mon_Row[i]+addrow;
+		Mon_Col[i]=((Mon_Col[i]+addcol==-1)||(Mon_Col[i]+addcol==15))? Mon_Col[i]:Mon_Col[i]+addcol;
+		cout<<"Mon_Row"<<i<<"="<<Mon_Row[i]<<"Mon_Col"<<i<<"="<<Mon_Col[i]<<endl;
 	}
-	cout<<"count"<<count<<endl;
+}
+// move all the monsters toward the direction of MEL
+void MoveMonster2(int Mon_Row[], int Mon_Col[], int count, int Mel_Row, int Mel_Col){
+	for (int i=0; i<count; i++){
+		int addrow=0, addcol=0;
+		addrow= ((Mel_Row-Mon_Row[i])>0)? 1:-1;
+		addcol= ((Mel_Col-Mon_Col[i])>0)? 1:-1;
+		Mon_Row[i]=((Mon_Row[i]+addrow==-1)||(Mon_Row[i]+addrow==15))? Mon_Row[i]:Mon_Row[i]+addrow;
+		Mon_Col[i]=((Mon_Col[i]+addcol==-1)||(Mon_Col[i]+addcol==15))? Mon_Col[i]:Mon_Col[i]+addcol;
+		//cout<<"Mon_Row"<<i<<"="<<Mon_Row[i]<<"Mon_Col"<<i<<"="<<Mon_Col[i]<<endl;
+		}
+}
+// move all the monsters backward the direction of MEL
+void Bomb(int Mon_Row[], int Mon_Col[], int count, int& Me_Row, int& Me_Col){
+	for (int i=0; i<count; i++){
+		int addrow=0, addcol=0;
+		int dis_row=Mon_Row[i]-Me_Row, dis_col=Mon_Col[i]-Me_Col;
+		if(dis_row<0)
+			addrow=-1;
+		else if(dis_row==0)
+			addrow=0;
+		else addrow=1;
+		if(dis_col<0)
+			addcol=-1;
+		else if(dis_col==0)
+			addcol=0;
+		else addcol=1;
+
+//		cout<<addrow<<" "<<addcol<<" ";
+		Mon_Row[i]=((Mon_Row[i]+addrow==-1)||(Mon_Row[i]+addrow==15))? Mon_Row[i]:Mon_Row[i]+addrow;
+		Mon_Col[i]=((Mon_Col[i]+addcol==-1)||(Mon_Col[i]+addcol==15))? Mon_Col[i]:Mon_Col[i]+addcol;
+		//cout<<"Mon_Row"<<i<<"="<<Mon_Row[i]<<"Mon_Col"<<i<<"="<<Mon_Col[i]<<endl;
+	}
+
 }
 
 // move the player
-void Move(int& Me_Row, int& Me_Col){
-	cout<<"Enter the direction"<<endl;
+char Move(int Mon_Row[], int Mon_Col[], int count, int& Me_Row, int& Me_Col, int& count_b){
+	cout<<"Enter the direction or use the TNT"<<endl;
 	char direction;
 	cin>>direction;
-	while ((direction!='a')&&(direction!='s')&&(direction!='d')&&(direction!='w')){
-		// cout
+	while ((direction!='a')&&(direction!='s')&&(direction!='d')&&(direction!='w')&&(direction!='b')){
+		cout<<"Give a right direction: ";
 		cin>>direction;
 	}
 	switch (direction){
-	case 'a':
-		Me_Col=(Me_Col==0)? Me_Col:Me_Col-1;
-		break;
-	case 's':
-		Me_Row=(Me_Row==14)? Me_Row:Me_Row+1;
-		break;
-	case 'd':
-		Me_Col=(Me_Col==14)? Me_Col:Me_Col+1;
-		break;
-	case 'w':
-		Me_Row=(Me_Row==0)? Me_Row:Me_Row-1;;
+		case 'a':
+			Me_Col=(Me_Col==0)? Me_Col:Me_Col-1;
+			break;
+		case 's':
+			Me_Row=(Me_Row==14)? Me_Row:Me_Row+1;
+			break;
+		case 'd':
+			Me_Col=(Me_Col==14)? Me_Col:Me_Col+1;
+			break;
+		case 'w':
+			Me_Row=(Me_Row==0)? Me_Row:Me_Row-1;
+			break;
+		case 'b':
+			if(count_b<4)
+				Bomb(Mon_Row,Mon_Col,count, Me_Row, Me_Col);
+			else
+				cout<<cout<<"No TNT Left!"<<endl;
+			count_b+=1;
+			break;
+
 	}
+	return direction;
 }
+
 // count how many monsters in a particular box
 int CountNum(int Mon_Row[], int Mon_Col[], int count, int row, int col){
 	int num=0;
 	for (int i=0; i<count; i++){
 		if ((Mon_Row[i]==row)&&(Mon_Col[i]==col)){
 			num=num+1;
-			cout<<"Mon_Row"<<i<<"="<<Mon_Row[i]<<"Mon_Col"<<i<<"="<<Mon_Col[i]<<endl;
+			//cout<<"Mon_Row"<<i<<"="<<Mon_Row[i]<<"Mon_Col"<<i<<"="<<Mon_Col[i]<<endl;
 		}
 	}
 	return num;
 
 }
 
+//construct MAP
 void Construct(int Map[][15],int Mon_Row[], int Mon_Col[], int count, int& Me_Row, int& Me_Col){
 	int mon_num=0;
-	//construct MAP
 	for (int i=0; i<15; i++){
 		for (int j=0; j<15; j++){
 			mon_num=CountNum(Mon_Row, Mon_Col, count,i,j);
--- LAB6.cpp
+++ LAB6.cpp
@@ -15,8 +15,8 @@ using namespace std;
 void CheckCrash(bool& crash, int count, int Me_Row, int Me_Col, int Mon_Row[], int Mon_Col[]);
 void NewMonster(int Mon_Row[], int Mon_Col[], int count, int Me_Row, int Me_Col);
 void MoveMonster(int Mon_Row[], int Mon_Col[], int count);
-void Move(int& Me_Row, int& Me_Col);
-//int CountNum(int Mon_Row[], int Mon_Col[], int count, int row, int col);
+//void MoveMonster2(int Mon_Row[], int Mon_Col[], int count, int Mel_Row, int Mel_Col);
+char Move(int Mon_Row[], int Mon_Col[], int count, int& Me_Row, int& Me_Col, int& count_b);
 void Construct(int Map[][15],int Mon_Row[], int Mon_Col[], int count, int& Me_Row, int& Me_Col);
 void Display(int Map[][15],int Mon_Row[], int Mon_Col[], int count, int& Me_Row, int& Me_Col);
 
@@ -25,6 +25,8 @@ int main() {
 	// Evil Monsters[225];
 	int Mon_Row[225], Mon_Col[225], Me_Row=14, Me_Col=0;
 	int count = 0;
+	int count_b=0; // number of bombs
+	char dir;
 	bool crash = false;
 	int Map[15][15];
 
@@ -32,21 +34,25 @@ int main() {
 	Display(Map,Mon_Row, Mon_Col,count,Me_Row,Me_Col);
 
 	while (crash == false){
-		cout<<"count: "<<count;
-		NewMonster(Mon_Row, Mon_Col, count, Me_Row, Me_Col);
-		count=count+1; cout<<"count: "<<count;
-		//MoveMonster(Mon_Row, Mon_Col, count);
+		//MoveMonster2(Mon_Row, Mon_Col, count, Me_Row, Me_Col);
 
-		Move(Me_Row, Me_Col); // cin direction
+		dir=Move(Mon_Row, Mon_Col, count, Me_Row, Me_Col, count_b); // cin direction or bomb
+		// in normal situation
+		if (dir!='b'){
+			cout<<"count: "<<count;
+			NewMonster(Mon_Row, Mon_Col, count, Me_Row, Me_Col);
+			count=count+1; cout<<"count: "<<count;
+			MoveMonster(Mon_Row, Mon_Col,count);
+		}
+		// check crash
 		CheckCrash(crash, count, Me_Row, Me_Col, Mon_Row, Mon_Col);
 		if(crash == true){
 			cout<<"Bomb!!!"<<endl;
 			break;
 		}
 		Construct(Map,Mon_Row,Mon_Col,count,Me_Row, Me_Col);
-		Display(Map,Mon_Row, Mon_Col,count,Me_Row,Me_Col);
+		Display(Map,Mon_Row, Mon_Col,count,Me_Row,Me_Col); //make a display
 	}
-
 	return 0;
-
 }
+
