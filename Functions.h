72ed8c634cc51a424310b5a6181365e8845ba0f6
ShirleyQ <myshirley@outlook.com>
2018-03-24 22:58:33 +0800

    NewMonster CheckCrash CountNum fixed; Construct and Display added

--- Functions.h
+++ Functions.h
@@ -1,7 +1,7 @@
 /*
  * Functions.h
  *
- *  Created on: 2018¦~3¤ë22¤é
+ *  Created on: 2018ï¿½~3ï¿½ï¿½22ï¿½ï¿½
  *      Author: Mystic
  */
 
@@ -16,21 +16,23 @@ using namespace std;
 void CheckCrash(bool& crash, int count, int Me_Row, int Me_Col, int Mon_Row[], int Mon_Col[]){
 	for (int i=0; i<=count; i++){
 		if ((Me_Row==Mon_Row[i])&&(Me_Col==Mon_Col[i])){
-			crash=false;
-			break;
+			crash=true;
 		}
 	}
+	cout<<"crash: "<<crash<<endl;
 }
-// create a new monster
-void NewMonster(int Mon_Row[], int Mon_Col[], int count){
+// create a new monster (dont put new monster on Mel)
+void NewMonster(int Mon_Row[], int Mon_Col[], int count, int Me_Row, int Me_Col){
 	int row=rand()%15;
 	int col=rand()%15;
-	while (((row==14)&&(col==0))||((row==0)&&(col==14))){
-		row=rand()%15;
-		col=rand()%15;
-	}
+//	if (((row==14)&&(col==0))||((row==0)&&(col==14))||((row==Me_Row)&&(col==Me_Col))){
+//		row=rand()%15;
+//		col=rand()%15;
+//	}
 	Mon_Row[count]=row;
 	Mon_Col[count]=col;
+	cout<<" new monster row:"<<Mon_Row[count]+1<<" col:"<<Mon_Col[count]+1<<endl;
+
 }
 
 void MoveMonster(int Mon_Row[], int Mon_Col[], int count){
@@ -44,6 +46,7 @@ void MoveMonster(int Mon_Row[], int Mon_Col[], int count){
 		Mon_Row[i]=((Mon_Row[i]+addrow==0)||(Mon_Row[i]+addrow==14))? Mon_Row[i]:Mon_Row[i]+addrow;
 		Mon_Col[i]=((Mon_Col[i]+addcol==0)||(Mon_Col[i]+addcol==14))? Mon_Col[i]:Mon_Col[i]+addcol;
 	}
+	cout<<"count"<<count<<endl;
 }
 
 // move the player
@@ -62,25 +65,51 @@ void Move(int& Me_Row, int& Me_Col){
 	case 's':
 		Me_Row=(Me_Row==14)? Me_Row:Me_Row+1;
 		break;
-	case 'f':
+	case 'd':
 		Me_Col=(Me_Col==14)? Me_Col:Me_Col+1;
 		break;
-	default:
+	case 'w':
 		Me_Row=(Me_Row==0)? Me_Row:Me_Row-1;;
 	}
 }
 // count how many monsters in a particular box
 int CountNum(int Mon_Row[], int Mon_Col[], int count, int row, int col){
 	int num=0;
-	for (int i=0; i<=count; i++){
-		if ((Mon_Row[count]==row)&&(Mon_Col[count]==col)){
+	for (int i=0; i<count; i++){
+		if ((Mon_Row[i]==row)&&(Mon_Col[i]==col)){
 			num=num+1;
+			cout<<"Mon_Row"<<i<<"="<<Mon_Row[i]<<"Mon_Col"<<i<<"="<<Mon_Col[i]<<endl;
 		}
 	}
 	return num;
+
 }
 
-void Display(){
+void Construct(int Map[][15],int Mon_Row[], int Mon_Col[], int count, int& Me_Row, int& Me_Col){
+	int mon_num=0;
+	//construct MAP
+	for (int i=0; i<15; i++){
+		for (int j=0; j<15; j++){
+			mon_num=CountNum(Mon_Row, Mon_Col, count,i,j);
+			Map[i][j]=mon_num;
+			}
+		}
+	Map[Me_Row][Me_Col]=9;
+}
+
+// show the display
+void Display(int Map[][15],int Mon_Row[], int Mon_Col[], int count, int& Me_Row, int& Me_Col){
+
+	//show MAP
+	for (int i=0; i<15; i++){
+			for (int j=0; j<15; j++){
+				if(Map[i][j]==0)
+					cout<<'|'<<" ";
+				else
+					cout<<'|'<<Map[i][j];
+			}
+			cout<<'|'<<endl;
+		}
 
 }
 
--- LAB6.cpp
+++ LAB6.cpp
@@ -13,11 +13,12 @@
 using namespace std;
 
 void CheckCrash(bool& crash, int count, int Me_Row, int Me_Col, int Mon_Row[], int Mon_Col[]);
-void NewMonster(int Mon_Row[], int Mon_Col[], int count);
+void NewMonster(int Mon_Row[], int Mon_Col[], int count, int Me_Row, int Me_Col);
 void MoveMonster(int Mon_Row[], int Mon_Col[], int count);
 void Move(int& Me_Row, int& Me_Col);
-int CountNum(int Mon_Row[], int Mon_Col[], int count, int row, int col);
-void Display();
+//int CountNum(int Mon_Row[], int Mon_Col[], int count, int row, int col);
+void Construct(int Map[][15],int Mon_Row[], int Mon_Col[], int count, int& Me_Row, int& Me_Col);
+void Display(int Map[][15],int Mon_Row[], int Mon_Col[], int count, int& Me_Row, int& Me_Col);
 
 int main() {
 	srand(time(NULL));
@@ -25,27 +26,26 @@ int main() {
 	int Mon_Row[225], Mon_Col[225], Me_Row=14, Me_Col=0;
 	int count = 0;
 	bool crash = false;
+	int Map[15][15];
+
+	Construct(Map,Mon_Row,Mon_Col,count,Me_Row, Me_Col);
+	Display(Map,Mon_Row, Mon_Col,count,Me_Row,Me_Col);
 
 	while (crash == false){
-		if (count!=0){
-			MoveMonster(Mon_Row, Mon_Col, count);
-		}
-		NewMonster(Mon_Row, Mon_Col, count);
-		CheckCrash(crash, count, Me_Row, Me_Col, Mon_Row, Mon_Col);
-		// display
-		if (crash==true){ break;}
-		Move(Me_Row, Me_Col);
-		//display
-		if (crash==true){ break;}
+		cout<<"count: "<<count;
+		NewMonster(Mon_Row, Mon_Col, count, Me_Row, Me_Col);
+		count=count+1; cout<<"count: "<<count;
+		//MoveMonster(Mon_Row, Mon_Col, count);
 
-		count=count+1;
+		Move(Me_Row, Me_Col); // cin direction
+		CheckCrash(crash, count, Me_Row, Me_Col, Mon_Row, Mon_Col);
+		if(crash == true){
+			cout<<"Bomb!!!"<<endl;
+			break;
+		}
+		Construct(Map,Mon_Row,Mon_Col,count,Me_Row, Me_Col);
+		Display(Map,Mon_Row, Mon_Col,count,Me_Row,Me_Col);
 	}
-	/*
-	int row = rand()%15, col = rand()%15;
-	cout<<"initial row is "<<row<<", initial col is "<<col<<".\n";
-	Evil Monster=Evil(row, col);
-	cout<<"Row is "<<Monster.NewRow(row)<<", Col is "<<Monster.NewCol(col)<<".\n";
-	*/
 
 	return 0;
 
